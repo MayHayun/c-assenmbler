2d04dbc0c1f2fdd794069c24a9207035b00ae85a
MayHayun <100352622+MayHayun@users.noreply.github.com>
2022-03-21 20:25:21 +0200

    Update list.c

--- list.c
+++ list.c
@@ -1,5 +1,6 @@
 #include "list.h"
 
+/* insert node a list */
 void insert(LIST *l,char input []){
   NODE *p = (NODE*)malloc(sizeof(NODE));
   char *value  = (char*)malloc(sizeof(char)*(strlen(input)+1));
@@ -14,17 +15,44 @@ void insert(LIST *l,char input []){
   return;
 }
 
-LIST* newList(){
-  LIST *head = (LIST*)malloc(sizeof(LIST));
-  return head;
-}
-
-NODE* findNode(LIST *l, char name[]){
+/* check if a given list has a given node */
+int has(LIST *l ,char mystr[]){
   NODE *p = l->head;
   while(p != NULL){
-    if(!strcmp(p->val,name))
-      return p;
+    if(!strcmp(p->val,mystr)){
+      return 1;
+    }
     p = p->next;
   }
-  return NULL;
+  return 0;
 }
+
+/* create new list */
+LIST* newList(){
+  LIST *l = (LIST*)malloc(sizeof(LIST));
+  return l;
+}
+/* print the given list */
+void printList(LIST *l){
+  NODE *p = l->head;
+  while(p != NULL){
+    printf(" name: %s, mac: %d, ext: %d,  ent: %d, labDec: %d\n",p->val,p->mac,p->ext,p->ent,p->labDec);
+    p = p->next;
+  }
+  printf("\n");
+}
+
+/* return a node by search his name in the list */
+NODE* getNode(LIST *l, char name[]){
+  NODE *p = l->head;
+  while(p != NULL){
+    //printf("%s\n",p->val);
+    if(!strcmp(p->val,name)){
+      return p;
+    }
+    p = p->next;
+  }
+  return p;
+}
+
+/* void freeList(LIST*) */

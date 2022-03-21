9543eb0d82be65c8c7a033988954c8cbbccb4116
MayHayun <100352622+MayHayun@users.noreply.github.com>
2022-03-21 20:19:39 +0200

    Update toSpecial.c

--- toSpecial.c
+++ toSpecial.c
@@ -3,25 +3,14 @@
 #include <string.h>
 #include <ctype.h>
 
-#include "toBinary.h"
+
+char binToHex(int num []);
 
 
-/*a func to create the output file - in hex base*/
-void output(WORD *head){
-  int address = 100;
-  FILE *output;
-  WORD *ptr = head;
-  output = fopen("output.ob","w");
-  fprintf(output,"\t\t\t%d\t%d\n",IC-100-DC,DC);
-  while(ptr != NULL && address <= IC){
-    binToSpecial(ptr->word,address,output);
-    ptr = ptr->next;
-    address++;
-  }
-}
 
 /*a func to get the num in he special base specified*/
-void binToSpecial (int num [],int address,FILE* output){
+void binToSpecial (int num [],int address,FILE* output)
+{
   int num1 [] = {0,0,0,0};
   int num2 [] = {0,0,0,0};
   int num3 [] = {0,0,0,0};
@@ -30,8 +19,11 @@ void binToSpecial (int num [],int address,FILE* output){
   int i,j;
   char a, b, c, d, e;
   int count = 0;
-  for(i=0; i<5; i++){
-    for(j=0;j<4;j++){
+
+  for(i = 0; i < 5; i++)
+  {
+    for(j = 0 ; j < 4 ; j++)
+    {
       if(i == 0)
         num1[j] = num[count];
       else if(i == 1)
@@ -52,44 +44,43 @@ void binToSpecial (int num [],int address,FILE* output){
   e = binToHex(num5);
   if(address < 1000)
     fprintf(output, "0%d\tA%c-B%c-C%c-D%c-E%c\n", address, e, d, c, b, a);
-  else{
+  else
     fprintf(output, "%d\tA%c-B%c-C%c-D%c-E%c\n", address, e, d, c, b, a);
-  }
 }
 
 /*a func to convert a num in binary to hex base*/
 char binToHex(int num []){
   if(num[3] == 0 && num[2] == 0 && num[1] == 0 && num[0] == 0) 
     return '0';
-  if(num[3] == 0 && num[2] == 0 && num[1] == 0 && num[0] == 1)
+  else if(num[3] == 0 && num[2] == 0 && num[1] == 0 && num[0] == 1)
     return '1';
-  if(num[3] == 0 && num[2] == 0 && num[1] == 1 && num[0] == 0)
+  else if(num[3] == 0 && num[2] == 0 && num[1] == 1 && num[0] == 0)
     return '2';
-  if(num[3] == 0 && num[2] == 0 && num[1] == 1 && num[0] == 1)
+  else if(num[3] == 0 && num[2] == 0 && num[1] == 1 && num[0] == 1)
     return '3';
-  if(num[3] == 0 && num[2] == 1 && num[1] == 0 && num[0] == 0)
+  else if(num[3] == 0 && num[2] == 1 && num[1] == 0 && num[0] == 0)
     return '4';
-  if(num[3] == 0 && num[2] == 1 && num[1] == 0 && num[0] == 1)
+  else if(num[3] == 0 && num[2] == 1 && num[1] == 0 && num[0] == 1)
     return '5';
-  if(num[3] == 0 && num[2] == 1 && num[1] == 1 && num[0] == 0)
+  else if(num[3] == 0 && num[2] == 1 && num[1] == 1 && num[0] == 0)
     return '6';
-  if(num[3] == 0 && num[2] == 1 && num[1] == 1 && num[0] == 1)
+  else if(num[3] == 0 && num[2] == 1 && num[1] == 1 && num[0] == 1)
     return '7';
-  if(num[3] == 1 && num[2] == 0 && num[1] == 0 && num[0] == 0)
+  else if(num[3] == 1 && num[2] == 0 && num[1] == 0 && num[0] == 0)
     return '8';
-  if(num[3] == 1 && num[2] == 0 && num[1] == 0 && num[0] == 1)
+  else if(num[3] == 1 && num[2] == 0 && num[1] == 0 && num[0] == 1)
     return '9';
-  if(num[3] == 1 && num[2] == 0 && num[1] == 1 && num[0] == 0)
+  else if(num[3] == 1 && num[2] == 0 && num[1] == 1 && num[0] == 0)
     return 'a';
-  if(num[3] == 1 && num[2] == 0 && num[1] == 1 && num[0] == 1)
+  else if(num[3] == 1 && num[2] == 0 && num[1] == 1 && num[0] == 1)
     return 'b';
-  if(num[3] == 1 && num[2] == 1 && num[1] == 0 && num[0] == 0)
+  else if(num[3] == 1 && num[2] == 1 && num[1] == 0 && num[0] == 0)
     return 'c';
-  if(num[3] == 1 && num[2] == 1 && num[1] == 0 && num[0] == 1)
+  else if(num[3] == 1 && num[2] == 1 && num[1] == 0 && num[0] == 1)
     return 'd';
-  if(num[3] == 1 && num[2] == 1 && num[1] == 1 && num[0] == 0)
+  else if(num[3] == 1 && num[2] == 1 && num[1] == 1 && num[0] == 0)
     return 'e';
-  if(num[3] == 1 && num[2] == 1 && num[1] == 1 && num[0] == 1)
+  else if(num[3] == 1 && num[2] == 1 && num[1] == 1 && num[0] == 1)
     return 'f';
   return '\0';
 }

17596f222dff9d0b061f2cf798074bf34eb7c8bc
MayHayun <100352622+MayHayun@users.noreply.github.com>
2022-03-21 20:22:51 +0200

    Update preProcess.h

--- preProcess.h
+++ preProcess.h
@@ -1,10 +1,13 @@
+#ifndef _preProcess_H_
+#define _preProcess_H_
 
-#include "utils.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#define CUT "\t\n "
+#define CUT1 "\t\n "
 #define MAX_LINE_LENGTH 81
 #define MAX_MACRO_COMMANDS 6
-
 
 typedef struct MACRO
 {
@@ -13,3 +16,9 @@ typedef struct MACRO
     int numOfCommands;
     struct MACRO *next; 
 }MACRO;
+
+MACRO *push(char macroName[], char macroCommands[][MAX_LINE_LENGTH], int numOfCommands, MACRO *head);
+void preProcessing(FILE *fileName, char *nameOfFile);
+struct MACRO* find(struct MACRO *head, char macroName[]);
+
+#endif
